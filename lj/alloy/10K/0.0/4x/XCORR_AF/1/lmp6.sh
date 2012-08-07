b24a0a04b4e3a1e58c2a285ef9e0535bbefddf07
jasonlarkin <jasonlarkin84@gmail.com>
2012-08-07 17:09:18 -0400

    added xcorr data

---
+++ lj/alloy/10K/0.0/4x/XCORR_AF/1/lmp6.sh
@@ -0,0 +1,11 @@
+##/opt/open-mpi/tcp-gnu41/bin/mpirun -np 4 $EXEPATH/lmp_generic < $RUNPATH/lmp.in.sed.6
+
+#!/bin/bash
+cd $PBS_O_WORKDIR
+module load openmpi-psm-gcc
+
+RUNPATH=/home/jason/lammps/LJ/alloy/10K/0.0/4x/XCORR_AF/1
+EXEPATH=/home/jason/lammps/lammps-2Nov10/src
+
+mpirun -np `cat $PBS_NODEFILE | wc -l` $EXEPATH/lmp_generic < $RUNPATH/lmp.in.sed.6 
+
---
+++ lj/alloy/10K/0.0/4x/XCORR_AF/1/lmp_submit.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp1.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp2.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp3.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp4.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp5.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp6.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp7.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp8.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp9.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp10.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp11.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp12.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp13.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp14.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp15.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp16.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp17.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp18.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp19.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp20.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp21.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp22.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp23.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp24.sh
+qsub -l walltime=12:00:00 -l nodes=1:ppn=2 lmp25.sh
